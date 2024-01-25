319d176caac7e4beacd71d5ebcac3c24e424f6f7
William Benedict <will@benedict.wtf>
2024-01-25 15:06:49 -0800

    Log reader now uses reverse-disk-reader

--- log_reader.py
+++ log_reader.py
@@ -1,7 +1,22 @@
+import disk_reader
+
 BASE_DIR = "/var/log"
 
 
-def readLogs(filename: str, lineCount: int, filter: str = None) -> str:
+def readLogs(filename: str, lineLimit: int, filterToken: str = None) -> list:
     # todo protect against directory traversal
     with open(f"{BASE_DIR}/{filename}", "r") as file: # Open for reading
-        return file.read()
+        lines = []
+        for line in disk_reader.reverseRead(f"{BASE_DIR}/{filename}"):
+            if len(lines) >= lineLimit:  # Could remove this calc by incrementing an i variable
+                return lines
+
+            filtered = filterLine(line, filterToken)
+            if filtered:
+                lines.append(filtered)
+
+        return lines
+
+
+def filterLine(line: str, filterToken: str) -> str:
+    return line
--- tests/log_reader_test.py
+++ tests/log_reader_test.py
@@ -3,9 +3,16 @@ import log_reader
 
 
 class TestDir(unittest.TestCase):
+    # This test will fail on any other machine. Might could pass in a mock disk_reader,
+    # but let's get this done first...
     def test_doStuff(self):
         res = log_reader.readLogs("system.log", 1)
-        self.assertTrue("AMPDeviceDiscoveryAgent" in res)
+        self.assertTrue("__thr_AMMuxedDeviceDisconnected" in res[0])
+        self.assertFalse("rdisk5s1" in res[0])
+
+        res = log_reader.readLogs("fsck_apfs.log", 1)
+        self.assertTrue("rdisk5s1" in res[0])
+        self.assertFalse("__thr_AMMuxedDeviceDisconnected" in res[0])
 
 
 if __name__ == '__main__':
