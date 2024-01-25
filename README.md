6f356cf6ff2e81c8cec6adc92a08e6794677263d
William Benedict <will@benedict.wtf>
2024-01-25 14:59:53 -0800

    Added implicit testing of unicode chars

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ flask --app log_endpoint run
 Some Macs have port 5000 occupied, so you have to work around it.
 
 There might be some user permission issues with /var/log...
+
+Assumptions
+All files are plain text UTF-8
--- tests/disk_reader_test.py
+++ tests/disk_reader_test.py
@@ -4,9 +4,10 @@ import disk_reader
 import log_reader
 
 
+# Handy doc for unicode tests: https://www.cl.cam.ac.uk/~mgk25/ucs/examples/UTF-8-test.txt
 class TestDir(unittest.TestCase):
     def test_reverseRead(self):
-        indexes = {0: "c", 1: "b", 2: "a"}
+        indexes = {0: "c", 1: "", 2: "a"}
         i = 0
         for line in  disk_reader.reverseRead("lines.txt"):
             char = indexes[i]
