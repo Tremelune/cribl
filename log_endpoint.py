53ca390dca3534c45878be95f73b0c94f80dca20
William Benedict <will@benedict.wtf>
2024-01-26 18:03:07 -0800

    Wired up input params and response from log_reader

--- log_endpoint.py
+++ log_endpoint.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask import request
+
+import log_reader
 
 app = Flask(__name__)
 
 
-@app.route("/logs")
+@app.route("/logs", methods=['GET'])
 def get_log():
-    return "2:15p tuesday - Finished the whole sandwich and am just over the moon about it!"
+    filename = request.args.get('filename')
+    limit = request.args.get('limit')
+    filter = request.args.get('filter')
+
+    # At this point we should check for good input and respond with intelligent messages/codes to ensure
+    # the caller knows exactly why the call failed...but for now we'll just explode and return whatever
+    # error message our system bubbles up from the depths.
+    limit = int(limit) if limit else log_reader.MAX_LINES
+
+    return log_reader.readLogs(filename, limit, filter)
 
 
 @app.route("/")
