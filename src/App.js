8ccf79c9b9c2562aded4b3b9871473ba0fb844ac
William Benedict <will@benedict.wtf>
2024-01-28 16:20:14 -0800

    Removed vestigial code...Nothing uses this!

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ import React, { useState, useEffect } from 'react';
 import SearchForm from './SearchForm';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-      fetch('http://127.0.0.1:5000/', { mode: 'no-cors'})
-          .then(response => response.status)
-          .then(data => console.log(data))
-          .then(body => setData(body))
-          .catch(error => console.error(error));
-  }, []);
-
-  console.log('I was triggered during componentDidMount')
   return (
     <div className="App">
       <header className="App-header">
